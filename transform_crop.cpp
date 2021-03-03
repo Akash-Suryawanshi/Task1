2a696cba873d6c7c74acce88506885968c0e6408
Akash-Suryawanshi <suryaakash7799@gmail.com>
2021-03-03 15:31:43 +0530

    made function

--- transform_crop.cpp
+++ transform_crop.cpp
@@ -5,28 +5,31 @@
 using namespace std;
 using namespace cv;
 
+
 vector<Point2f>src_pts;
 vector<Point2f>dest_pts;
+
 
 void getPoint(int event, int x, int y, int flags, void* userdata) {
 	if (event == EVENT_LBUTTONDOWN && src_pts.size() < 4) {
 		Point2f p(x, y);
 		src_pts.push_back(p);
 	}
-}
+} 
+
 void init() {
 	dest_pts.push_back(Point2f(472, 52));
 	dest_pts.push_back(Point2f(472, 830));
 	dest_pts.push_back(Point2f(800, 830));
 	dest_pts.push_back(Point2f(800, 52));
 }
+ 
+void transform_and_crop(string myimage) {
+    init();
 
-int main() {
-	init();
+    Mat img = imread(myimage, IMREAD_GRAYSCALE);
 
-	Mat img = imread("traffic.jpg", IMREAD_GRAYSCALE);
-
-	if (img.empty()) return 0;
+	if (img.empty()) return ;
 
 	namedWindow("Image", 1);	
 
@@ -57,5 +60,12 @@ int main() {
 	imwrite("CroppedImage.jpg", cropedImage);
 	waitKey(0);
 	
-	return 0;
+	return ;
 }
+
+
+
+
+
+
+
