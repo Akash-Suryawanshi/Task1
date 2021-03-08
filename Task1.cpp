37cdde3836c4c1b18e0e6f8d07b3ed79c0e5f14a
Akash-Suryawanshi <suryaakash7799@gmail.com>
2021-03-09 01:19:20 +0530

    moving-density

--- Task1.cpp
+++ Task1.cpp
@@ -9,7 +9,7 @@ using namespace cv;
 
 int main() {
 	pair <vector<double>, vector<double>>densities;
-	densities = readVideo("trafficvideo.mp4");
+	densities = readVideo("tfv.mp4");
 	vector<double>QueueDensity = densities.first;
 	vector<double>DynamicDensity = densities.second;
 
