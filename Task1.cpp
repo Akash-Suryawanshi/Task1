07332d4ea3512108b764aca549ee7bb717907fc1
Akash-Suryawanshi <suryaakash7799@gmail.com>
2021-03-09 00:57:24 +0530

    convex-hull

--- Task1.cpp
+++ Task1.cpp
@@ -9,10 +9,9 @@ using namespace cv;
 
 int main() {
 	pair <vector<double>, vector<double>>densities;
-	densities = readVideo("trafficvideo.mp4");
+	densities = readVideo("tfv.mp4");
 	vector<double>QueueDensity = densities.first;
 	vector<double>DynamicDensity = densities.second;
-
 	cout << "framenum" << '\t' << '\t' << "queue density" << '\t' << '\t' << "dynamic density";
 	cout << endl;
 	for (int i = 0; i < QueueDensity.size(); i++) {
